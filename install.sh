c696ccc8582165d67204ef5277ff5d778f45ef2f
James Jarvis <jj@aprsworld.com>
2017-02-28 23:59:46 +0000

    Pulling together now

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp addDataPartiion /usr/local/sbin
+cp purgeHistory /usr/local/sbin
+cp purgeLogLocal /usr/local/sbin
+cp root-rw /usr/local/sbin
+cp root-ro /usr/local/sbin
+cp sethostname /usr/local/sbin
+# cp systemd/splashOLED.service
+cp etc/vimrc.local /etc/vim
+# cp bash.bashrc
